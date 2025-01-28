742b5c8868b2feb6979f18c5028f1192e4d46a3a
Stephen Mann <stephenfmann@gmail.com>
2025-01-28 09:34:43 +0100

    Switch to pytest for tests

--- evoke/tests/run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# Discover and run all tests in the 'tests' directory
-loader = unittest.TestLoader()
-suite = loader.discover(start_dir='evoke/tests/', pattern='test_*.py')
-
-runner = unittest.TextTestRunner()
-runner.run(suite)
